1afa993a99f78889a6de9d0a7960f1638ad2bfc8
Dohny42 <dohny42@gmail.com>
2026-05-04 14:53:31 +0200

    add basic raw calculation, multiple monitors support

--- disk_usage_calc_app/core.py
+++ disk_usage_calc_app/core.py
@@ -1,15 +1,15 @@
 from dataclasses import dataclass
 
 import psutil
-from psutil._common import bytes2human
+from screeninfo import get_monitors
 
 
 @dataclass
 class DriveInfo:
     name: str
-    total: str
-    used: str
-    free: str
+    total: int
+    used: int
+    free: int
     percent: float
 
 
@@ -20,11 +20,33 @@ def get_available_drives() -> dict[str, DriveInfo]:
             usage = psutil.disk_usage(partition.mountpoint)
             drives[partition.device] = DriveInfo(
                 name=partition.device,
-                total=bytes2human(usage.total),
-                used=bytes2human(usage.used),
-                free=bytes2human(usage.free),
+                total=usage.total,
+                used=usage.used,
+                free=usage.free,
                 percent=usage.percent,
             )
         except PermissionError:
             continue
     return drives
+
+
+def get_available_monitors() -> dict[str, tuple[int, int]]:
+    monitors = {}
+    for monitor in get_monitors():
+        monitors[monitor.name] = (monitor.width, monitor.height)
+    return monitors
+
+
+def calculate_disk_usage(
+    sampling_rate: int, retain_time: int, monitors_info: list[tuple[int, int]], available_space: int
+) -> tuple[bool, int]:
+    # return True if we can retain that much data, False otherwise
+    # assume each frame is 4 bytes per pixel (RGBA)
+    bytes_per_frame = 4 * sum(width * height for width, height in monitors_info)
+    frames_per_second = 1 / sampling_rate
+    bytes_per_second = bytes_per_frame * frames_per_second
+    bytes_per_day = bytes_per_second * 60 * 60 * 24
+    total_bytes_needed = bytes_per_day * retain_time
+    return total_bytes_needed <= available_space - (available_space * 0.05), int(
+        total_bytes_needed
+    )  # 5% reservation, could be configurable
--- disk_usage_calc_app/ui.py
+++ disk_usage_calc_app/ui.py
@@ -1,20 +1,71 @@
+from dataclasses import dataclass, field
+
 import streamlit as st
-from core import get_available_drives
+from core import DriveInfo, calculate_disk_usage, get_available_drives, get_available_monitors
+from psutil._common import bytes2human
 
 
-def format_drive_info(drive_info) -> None:
+@dataclass
+class CalculationOptions:
+    sampling_rate: int = 5
+    retain_time: int = 60
+    monitors_info: list[tuple[int, int]] = field(default_factory=list)
+
+
+def format_drive_info(drive_info: DriveInfo) -> None:
     st.progress(drive_info.percent / 100)
-    st.write(f"**Total Space:** {drive_info.total}")
-    st.write(f"**Used Space:** {drive_info.used}")
-    st.write(f"**Free Space:** {drive_info.free}")
+    st.write(f"**Total Space:** {bytes2human(drive_info.total)}")
+    st.write(f"**Used Space:** {bytes2human(drive_info.used)}")
+    st.write(f"**Free Space:** {bytes2human(drive_info.free)}")
     st.write(f"**Usage Percentage:** {drive_info.percent}%")
 
 
-def show_main_ui() -> None:
-    st.title("Disk Usage Calculator")
+def show_calculation_options() -> CalculationOptions:
+    st.subheader("Disk Usage Calculation Options")
+    sampling_rate = st.number_input("Sampling rate (seconds)", min_value=1, value=5, step=1)
+    retain_time = st.number_input("Retention time (days)", min_value=1, value=60, step=1)
+    available_monitors = get_available_monitors()
+    monitors_selected = st.multiselect(
+        "Select monitors to include in the calculation",
+        options=available_monitors.keys(),
+        default=available_monitors.keys(),
+    )
+    monitors_info = [available_monitors[monitor] for monitor in monitors_selected]
+    return CalculationOptions(
+        sampling_rate=sampling_rate, retain_time=retain_time, monitors_info=monitors_info
+    )
 
-    available_drives = get_available_drives()
-    drive_option = st.selectbox("Select an drive", options=available_drives.keys())
-    if drive_option:
-        drive_info = available_drives[drive_option]
-        format_drive_info(drive_info)
+
+def show_main_ui() -> None:
+    st.title("Observer Disk Usage Calculator")
+
+    configuration_col, results_col = st.columns(2, gap="xlarge", border=True)
+
+    with configuration_col:
+        # display available drives
+        available_drives = get_available_drives()
+        drive_option = st.selectbox("Select a drive", options=available_drives.keys())
+        if drive_option:
+            drive_info = available_drives[drive_option]
+            format_drive_info(drive_info)
+
+        st.divider()
+
+        # options for calculation
+        calculation_options = show_calculation_options()
+
+    with results_col:
+        st.subheader("Calculation Results")
+        if drive_option and calculation_options:
+            drive_info = available_drives[drive_option]
+            can_retain, needed_space = calculate_disk_usage(
+                sampling_rate=calculation_options.sampling_rate,
+                retain_time=calculation_options.retain_time,
+                monitors_info=calculation_options.monitors_info,
+                available_space=drive_info.free,
+            )
+            if can_retain:
+                st.success("The drive has enough space to retain the data.")
+            else:
+                st.error("The drive does NOT have enough space to retain the data.")
+            st.write(f"**Estimated Space Needed:** {bytes2human(needed_space)}")
